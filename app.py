8358a3fa5fd998a5bdf8cf021e8f4dd51be81eb6
JPedroCMartins <jpedro7682@gmail.com>
2025-10-25 21:15:57 -0400

    count

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask, send_from_directory
+from flask import Flask, redirect, url_for, send_from_directory
+import json
+import os
 
 app = Flask(__name__, static_folder="static")
 
 @app.route('/')
 def serve_index():
     return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/qr_code')
+def qr_code():
+    # Caminho do arquivo de acesso
+    caminho_arquivo = 'acesso.json'
+
+    # Se o arquivo existir, lê o número atual de acessos
+    if os.path.exists(caminho_arquivo):
+        with open(caminho_arquivo, 'r') as f:
+            dados = json.load(f)
+            acessos = dados.get('acessos', 0)
+    else:
+        acessos = 0
+
+    # Soma +1
+    acessos += 1
+
+    # Salva o novo valor
+    with open(caminho_arquivo, 'w') as f:
+        json.dump({'acessos': acessos}, f, indent=2)
+
+    # Redireciona para a função serve_index
+    return redirect(url_for('serve_index'))
 
 @app.route('/<path:path>')
 def serve_static(path):
